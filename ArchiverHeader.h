45a64358ad213815aa2b99cd9e46147df865ad0d
FattyKitty <tony.shumlin@gmail.com>
2021-03-13 12:35:00 +0300

    Added few comments

--- ArchiverHeader.h
+++ ArchiverHeader.h
@@ -1,4 +1,4 @@
-#define ALLOW 00666
+#define ALLOW 00666 //code of restriction to open and write to folder
 #define MAXFILENAME 50
 
 #include <stdio.h>
@@ -9,7 +9,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int PackArchive(char *FilePath, char* ArchiverPath, int depth);
-int PackFile(char *FilePath, int OpenArchive, int depth, char *FolderName);
-int UnpackFile(char *FileName, int size, int ArchiveDescr);
-int UnpackArchive(char *ArchivePath, int depth, char *ZipFolder);
+int PackArchive(char *FilePath, char* ArchiverPath, int depth); //packing folder
+int PackFile(char *FilePath, int OpenArchive, int depth, char *FolderName); //packing one sigle file
+int UnpackFile(char *FileName, int size, int ArchiveDescr); //unpacking one single file
+int UnpackArchive(char *ArchivePath, int depth, char *ZipFolder); //unpacking one or multiple folders
